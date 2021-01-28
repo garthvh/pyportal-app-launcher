8814ccb1771a581271bccbdd374bd141899b14c3
Garth Vander Houwen <garthvh@yahoo.com>
2021-01-27 23:33:12 -0800

    Update pyportal-app-launcher.py
    Update for 2x3 titano 480x320

--- pyportal-app-launcher.py
+++ pyportal-app-launcher.py
@@ -3,6 +3,7 @@ import board
 import adafruit_touchscreen
 from adafruit_pyportal import PyPortal
 
+import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
  
@@ -10,11 +11,13 @@ keyboard_active = False
 
 #Initiallize the Keyboard
 try:
-  kbd = Keyboard()
-  keyboard_active = True
+    kbd = Keyboard(usb_hid.devices)
+    keyboard_active = True
 except OSError:
   keyboard_active = False
 
+WIDTH = board.DISPLAY.width
+HEIGHT = board.DISPLAY.height
 
 # the current working directory (where this file is)
 cwd = ("/"+__file__).rsplit('/', 1)[0]
@@ -23,7 +26,7 @@ cwd = ("/"+__file__).rsplit('/', 1)[0]
 pyportal = PyPortal(url='',
                     json_path='',
                     status_neopixel=board.NEOPIXEL,
-                    default_bg=cwd+"/button-template-12.BMP",
+                    default_bg=cwd+"/button-template-6.BMP",
                     text_font=cwd+"/fonts/Collegiate-50.bdf",
                     text_position=((100, 129), (155, 180)),
                     text_color=(0x000000, 0x000000),
@@ -59,74 +62,48 @@ while True :
         y = (p_list[1][1]+p_list[2][1])/2
         print(x,y)
 
-
-
         # I will refer the the icon/button matrix using the following grid.
         # A1 is the icon/button in the top left corner looking at the PyPortal
-        # D3 is the icon/button in the bottom right corner
-        # +---+----+----+----+----+
-        # |   | A  | B  | C  | D  |
-        # +---+----+----+----+----+
-        # | 1 | A1 | B1 | C1 | D1 |
-        # | 2 | A2 | B2 | C2 | D2 |
-        # | 3 | A3 | B3 | C3 | D3 |
-        # +---+----+----+----+----+
+        # C2 is the icon/button in the bottom right corner
+        # +---+----+----+----+
+        # |   | A  | B  | C  |
+        # +---+----+----+----+
+        # | 1 | A1 | B1 | C1 |
+        # | 2 | A2 | B2 | C2 | 
+        # +---+----+----+----+
 
 
         # For each button/icon pressed we send a keyboard command using kbd.send
         # this can send multiple key presses as if you were pressing them all at the same time
-        # I use a mac and have configured each one of my shortcuts to open an application 
         # using ALT+Control+Shift+Command+[some letter] 
-        # These shortcuts trigger an application to launch that was configured using Automator 
-
 
         # Column A
-        if x > 22.5 and x < 72.5:
+        if x > 22.5 and x < 147.5:
 
           # Icon/Button A1
-          if y > 25 and y < 75:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.V)  
+          if y > 20 and y < 145:
+            kbd.send(Keycode.CONTROL,Keycode.SHIFT, Keycode.M)  
           # Icon/Button A2
-          elif y > 100 and y < 150:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.G)
-          # Icon/Button A3  
-          elif y > 175 and y < 225:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.W)  
+          elif y > 175 and y < 300:
+            kbd.send(Keycode.CONTROL,Keycode.ALT, Keycode.DELETE)
 
         # Column B
-        elif x > 97.5 and x < 147.5:
+        elif x > 177.5 and x < 302.5:
           # Icon/Button B1 
-          if y > 25 and y < 75:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.S) 
+          if y > 20 and y < 145:
+            kbd.send(Keycode.CONTROL,Keycode.ALT, Keycode.DELETE)
           # Icon/Button B2
-          elif y > 100 and y < 150:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.F)
-          # Icon/Button B3 
-          elif y > 175 and y < 225:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.X)  
-        # Column C
-        elif x > 172.5 and x < 222.5:
-          # Icon/Button C1 
-          if y > 25 and y < 75:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.C)
-          # Icon/Button C2
-          elif y > 100 and y < 150:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.T)
-          # Icon/Button C3
-          elif y > 175 and y < 225:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.A)
+          elif y > 175 and y < 300:
+            kbd.send(Keycode.CONTROL,Keycode.ALT, Keycode.DELETE)
 
-        # Column D 
-        elif x > 247.5 and x < 297.5:
-          # Icon/Button D1
-          if y > 25 and y < 75:
-            kbd.send(Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.T)  
-          # Icon/Button D2
-          elif y > 100 and y < 150:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.Z)  
-          # Icon/Button D3
-          elif y > 175 and y < 225:
-            kbd.send(Keycode.ALT,Keycode.CONTROL, Keycode.COMMAND,Keycode.SHIFT,Keycode.P)  
+        # Column C
+        elif x > 332.5 and x < 457.5:
+          # Icon/Button C1 
+          if y > 20 and y < 145:
+            kbd.send(Keycode.CONTROL,Keycode.ALT, Keycode.DELETE)
+          # Icon/Button C2
+          elif y > 175 and y < 300:
+            kbd.send(Keycode.CONTROL,Keycode.ALT, Keycode.DELETE)
 
         # clear list for next detection
         p_list = []
